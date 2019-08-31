2379863781cf17809596fecc69141bb531d6e18b
zhangbo340 <1>
2019-08-31 22:53:01 +0800

    接口扩充

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,20 +1,46 @@
 package com.example.demo.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.example.demo.entity.User;
+import com.example.demo.mapper.UserMapper;
+import com.example.demo.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
  *  前端控制器
- * </p>
+ * </p>1
  *
  * @author zhagnbo284
  * @since 2019-08-31
  */
-@Controller
-@RequestMapping("/user")
+@RestController
+
 public class UserController {
+    @Autowired
+    private IUserService a;
+    @RequestMapping("/getUser")
+    public User getUser() {
+        User  user=a.getById("1");
+        return user;
+    }
+
+
+    @RequestMapping("/getUsers")
+    public List<User> getUsers() {
+        List<User>  users=a.list();
+        return users;
+    }
+
+
+
+
 
 }
