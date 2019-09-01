00479f560ff31146fb0fb3d48fbf3d08014699bb
zhangbo340 <1>
2019-09-01 15:45:46 +0800

    基本实现了

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,10 +2,13 @@ package com.example.demo.controller;
 
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.demo.entity.User;
 import com.example.demo.mapper.UserMapper;
 import com.example.demo.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
@@ -26,7 +29,10 @@ import java.util.List;
 public class UserController {
     @Autowired
     private IUserService a;
+    @Autowired
+    private UserMapper b1;
     @RequestMapping("/getUser")
+    @Transactional
     public User getUser() {
         User  user=a.getById("1");
         return user;
@@ -34,10 +40,52 @@ public class UserController {
 
 
     @RequestMapping("/getUsers")
+    @Transactional
     public List<User> getUsers() {
         List<User>  users=a.list();
         return users;
     }
+
+
+    @RequestMapping("/addUser")
+    @Transactional
+    public boolean addUser() {
+        User tmp=new User();
+        tmp.setName("12");
+        tmp.setAge(12);
+        boolean  u=a.save(tmp );
+        return u;
+    }
+
+    @RequestMapping("/delUser")
+    @Transactional
+    public boolean delUser() {
+        User tmp=new User();
+        tmp.setName("12");
+        tmp.setAge(12);
+        boolean  u=a.removeById(1 );
+        return u;
+    }
+
+    @RequestMapping("/update")
+    public boolean update() {
+        User tmp=new User();
+        tmp.setId(1);
+        tmp.setName("55");
+        tmp.setAge(67);
+        System.out.println(tmp.getId());
+        boolean  u=a.save(tmp );  //为什么不写id?????qiguai .
+        return u;
+    }
+
+
+
+
+
+
+
+
+
 
 
 
--- src/main/java/com/example/demo/mapper/UserMapper.java
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package com.example.demo.mapper;
 
 import com.example.demo.entity.User;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * <p>
@@ -11,6 +12,10 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @author zhagnbo284
  * @since 2019-08-31
  */
+@Mapper
 public interface UserMapper extends BaseMapper<User> {
+
+
+
 
 }
